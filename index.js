427354a0226cb986a152ddeea912f3f2f49842ef
hcgonro <hcgonro@gmail.com>
2026-05-22 12:04:18 +0200

    ajustes varios

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const tareaCardCrear = document.querySelector('.tarea-card-crear');
     const btnNuevaTarea = document.querySelector('.btn-nueva-tarea');
     const btnCancelar = document.querySelector('.tarea-btn-cancelar');
+    const desplegablePrioridades = tareaCardCrear.querySelector('.tarea-prioridades');
 
     btnNuevaTarea.addEventListener('click', function() {
         tareaCardCrear.style.display = 'flex';
@@ -16,6 +17,21 @@ document.addEventListener("DOMContentLoaded", function() {
         tareaCardCrear.querySelector('.tarea-input-fecha').value = '';
         tareaCardCrear.querySelector('.tarea-prioridades').selectedIndex = 0;
         tareaCardCrear.querySelector('.tarea-input-texto').value = '';
+    })
+
+    desplegablePrioridades.addEventListener('change', function() {
+        const opcionSeleccionada = desplegablePrioridades.options[desplegablePrioridades.selectedIndex];
+        const prioridadSeleccionada = opcionSeleccionada.value;
+        switch (prioridadSeleccionada) {
+            case 'prioridad1':
+                desplegablePrioridades.style.backgroundColor = 'greenyellow';
+                break;
+            case 'prioridad2':
+                desplegablePrioridades.style.backgroundColor = 'red';
+                break;
+            case 'prioridad3':
+                desplegablePrioridades.style.backgroundColor = 'blue';
+        }   
     })
 
     tareaCardCrear.style.display = 'none';
