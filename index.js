ae3e2c4bb9d7986b3e6b695b5cad1eff82787f0e
hegondev <hcgonro@gmail.com>
2026-05-24 21:18:02 +0200

    arreglos varios

--- index.js
+++ index.js
@@ -4,36 +4,52 @@ document.addEventListener("DOMContentLoaded", function() {
     const btnNuevaTarea = document.querySelector('.btn-nueva-tarea');
     const btnCancelar = document.querySelector('.tarea-btn-cancelar');
     const desplegablePrioridades = tareaCardCrear.querySelector('.tarea-prioridades');
+    
+    /* Gestión lista de tareas en memoria local al iniciar la webapp */
+    let listaTareasMemoria = localStorage.getItem("tareasGuardadas");
+    if(listaTareasMemoria) {
+        // Visualización de las tareas de la lista
+        
+    }
+    /* Fin de gestión de lista de tareas en memoria local*/
 
     btnNuevaTarea.addEventListener('click', function() {
+        // Mostrar card para crear tarea y ocultar botón añadir tarea
         tareaCardCrear.style.display = 'flex';
         btnNuevaTarea.style.display = 'none';
     })
 
     btnCancelar.addEventListener('click', function() {
+        // Mostrar botón añadir tarea y ocultar card para crear tarea
         tareaCardCrear.style.display = 'none';
         btnNuevaTarea.style.display = 'flex';
 
+        // Restaurar parámetros a valores iniciales
         tareaCardCrear.querySelector('.tarea-input-fecha').value = '';
-        tareaCardCrear.querySelector('.tarea-prioridades').selectedIndex = 0;
         tareaCardCrear.querySelector('.tarea-input-texto').value = '';
+        desplegablePrioridades.selectedIndex = 0;
+        desplegablePrioridades.style.backgroundColor = 'transparent';
     })
 
     desplegablePrioridades.addEventListener('change', function() {
-        const opcionSeleccionada = desplegablePrioridades.options[desplegablePrioridades.selectedIndex];
-        const prioridadSeleccionada = opcionSeleccionada.value;
-        switch (prioridadSeleccionada) {
-            case 'prioridad1':
+        // Cambiamos color según opción seleccionada
+        const opcionSeleccionada = desplegablePrioridades.selectedIndex;
+        switch (opcionSeleccionada) {
+            case 1:
                 desplegablePrioridades.style.backgroundColor = 'greenyellow';
                 break;
-            case 'prioridad2':
+            case 2:
                 desplegablePrioridades.style.backgroundColor = 'red';
                 break;
-            case 'prioridad3':
+            case 3:
                 desplegablePrioridades.style.backgroundColor = 'blue';
         }   
     })
 
+    // Al iniciar:
+    // No mostramos card de crear tarea
     tareaCardCrear.style.display = 'none';
+    // Prioridad inicial a 0
+    desplegablePrioridades.selectedIndex = 0;
 
 });
